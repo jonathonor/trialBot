d524764fdc6f3465d86c3578d62f045db1200262
BaBaBoi <117912012+BaBaBoi2222@users.noreply.github.com>
2023-02-16 22:47:43 +0530

    Update trialbot-lowdb.js

--- trialbot-lowdb.js
+++ trialbot-lowdb.js
@@ -215,5 +215,6 @@ client.login(config.token);
 
 process.on('SIGINT', function () { 
     console.log('shutting down schedulers');
-    schedule.gracefulShutdown().then(() => process.exit(0))
+        schedule.gracefulShutdown()
+    .then(() => process.exit(0))
 });
